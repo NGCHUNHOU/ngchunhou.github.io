74d80119ec9887c8e79c933913f8f8e08e5eb6d1
hou <chunhou574197@gmail.com>
2023-03-20 14:46:05 +0800

    bootstrapping site

---
+++ src/pages/components/Body.tsx
@@ -0,0 +1,9 @@
+function Body() {
+    return (
+        <>
+            <div className="body">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit, eos est rerum harum vero possimus, iusto, animi facere quo hic natus eligendi officiis repudiandae excepturi corporis at rem! Dolorum, quibusdam.</div>
+        </>
+    )
+
+}
+export default Body
