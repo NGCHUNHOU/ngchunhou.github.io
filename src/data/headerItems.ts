88958f705175d0cc10a7f82f98388db0c9243c57
NGCHUNHOU <chunhou574197@gmail.com>
2025-08-21 16:32:25 +0800

    functional component uppercase first occurrence

--- src/data/headerItems.ts
+++ src/data/headerItems.ts
@@ -1,6 +1,6 @@
 const topBarItems = [
-    {name: "PERSONAL INFO", pageTitle: "Personal Infos", href: "/personal_infos"}, 
-    {name: "TOOLS", pageTitle: "Tools", href: "/tools"}, 
-    {name: "CONTACTS", pageTitle: "Contacts", href: "/contacts"}
+    {name: "PERSONAL INFO", pageTitle: "Personal Infos", href: "/Personal_infos"}, 
+    {name: "TOOLS", pageTitle: "Tools", href: "/Tools"}, 
+    {name: "CONTACTS", pageTitle: "Contacts", href: "/Contacts"}
 ]
 export default topBarItems
